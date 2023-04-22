6db865ca657829fe15a768cb04101b11fcd374b6
Chris Crane <chrisjcrane@gmail.com>
2023-04-22 15:30:18 -0600

    create mapquest facade specfile

--- spec/services/map_quest_spec.rb
+++ spec/services/map_quest_spec.rb
@@ -22,20 +22,6 @@ RSpec.describe MapQuestService do
           expect(locations.first[:latLng][:lng]).to eq(-104.99202)
         end
       end
-      
-      # it "returns coordinates for a given address" do
-      #   search = MapQuestService.new.address_to_coordinates("Denver, CO")
-
-      #   expect(search).to be_a(Hash)
-        
-      #   expect(search).to have_key(:lat)
-      #   expect(search[:lat]).to be_a(Float)
-      #   expect(search[:lat]).to eq(39.74001)
-
-      #   expect(search).to have_key(:lng)
-      #   expect(search[:lng]).to be_a(Float)
-      #   expect(search[:lng]).to eq(-104.99202)
-      # end
     end
   end
 end
