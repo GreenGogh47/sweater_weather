e73239b29a7a1a1a6e122080d22a74c60fcb13ec
Chris Crane <chrisjcrane@gmail.com>
2023-04-22 15:10:50 -0600

    install pry

--- spec/services/map_quest_spec.rb
+++ spec/services/map_quest_spec.rb
@@ -4,23 +4,25 @@ RSpec.describe MapQuestService do
   describe "instance methods" do
     describe "address_to_coordinates" do
       it "returns coordinates for a given address" do
-        search = MapQuestService.new.address_to_coordinates("Denver, CO")
-        expect(search).to be_a(Hash)
-        expect(search).to have_key(:info)
-        expect(search).to have_key(:results)
+        VCR.use_cassette("mapquest_denver_lat_lng") do
+          search = MapQuestService.new.address_to_coordinates("Denver, CO")
+          expect(search).to be_a(Hash)
+          expect(search).to have_key(:info)
+          expect(search).to have_key(:results)
 
-        require 'pry'; binding.pry
+          require 'pry'; binding.pry
 
-        locations = search[:results][:locations]
+          locations = search[:results][:locations]
 
-        expect(locations).to be_an(Array)
-        expect(locations.first).to have_key(:latLng)
+          expect(locations).to be_an(Array)
+          expect(locations.first).to have_key(:latLng)
 
-        expect(locations.first[:latLng]).to have_key(:lat)
-        expect(locations.first[:latLng][:lat]).to eq(39.74001)
+          expect(locations.first[:latLng]).to have_key(:lat)
+          expect(locations.first[:latLng][:lat]).to eq(39.74001)
 
-        expect(locations.first[:latLng]).to have_key(:lng)
-        expect(locations.first[:latLng][:lng]).to eq(-104.99202)
+          expect(locations.first[:latLng]).to have_key(:lng)
+          expect(locations.first[:latLng][:lng]).to eq(-104.99202)
+        end
       end
       
       # it "returns coordinates for a given address" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,6 +98,6 @@ RSpec.configure do |config|
   VCR.configure do |config|
     config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
     config.hook_into :webmock
-    config.filter_sensitive_data("<MAPQUEST_API_KEY>") { ENV["MAPQUEST_API_KEY"] }
+    config.filter_sensitive_data("MAPQUEST_API_KEY") { ENV["MAPQUEST_API_KEY"] }
   end
 end
