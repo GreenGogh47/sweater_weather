8ab54225b702c83c43d3ef195a67b08ab7600549
Chris Crane <chrisjcrane@gmail.com>
2023-04-22 14:40:29 -0600

    modify initial service test

--- spec/services/map_quest_spec.rb
+++ spec/services/map_quest_spec.rb
@@ -5,17 +5,38 @@ RSpec.describe MapQuestService do
     describe "address_to_coordinates" do
       it "returns coordinates for a given address" do
         search = MapQuestService.new.address_to_coordinates("Denver, CO")
-
         expect(search).to be_a(Hash)
-        
-        expect(search).to have_key(:lat)
-        expect(search[:lat]).to be_a(Float)
-        expect(search[:lat]).to eq(39.74001)
+        expect(search).to have_key(:info)
+        expect(search).to have_key(:results)
 
-        expect(search).to have_key(:lng)
-        expect(search[:lng]).to be_a(Float)
-        expect(search[:lng]).to eq(-104.99202)
+        locations = search[:results][:locations]
+
+        expect(locations).to be_an(Array)
+        expect(locations.first).to have_key(:latLng)
+
+        expect(locations.first[:latLng]).to have_key(:lat)
+        expect(locations.first[:latLng][:lat]).to eq(39.74001)
+
+        expect(locations.first[:latLng]).to have_key(:lng)
+        expect(locations.first[:latLng][:lng]).to eq(-104.99202)
       end
+      
+      
+      
+      
+      # it "returns coordinates for a given address" do
+      #   search = MapQuestService.new.address_to_coordinates("Denver, CO")
+
+      #   expect(search).to be_a(Hash)
+        
+      #   expect(search).to have_key(:lat)
+      #   expect(search[:lat]).to be_a(Float)
+      #   expect(search[:lat]).to eq(39.74001)
+
+      #   expect(search).to have_key(:lng)
+      #   expect(search[:lng]).to be_a(Float)
+      #   expect(search[:lng]).to eq(-104.99202)
+      # end
     end
   end
 end
