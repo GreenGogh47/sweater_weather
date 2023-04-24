44ae9916b3fd9de7a48bbbfc58f5eead82798f31
Chris Crane <chrisjcrane@gmail.com>
2023-04-24 10:58:27 -0600

    15 min mark and complete salary info portion of facade

--- app/facades/salaries_facade.rb
+++ app/facades/salaries_facade.rb
@@ -1,6 +1,17 @@
 class SalariesFacade
-  def initialize(city)
+  
+  def self.salary_info(city)
     @service = TeleportService.new.get_salaries(city)
-    require 'pry'; binding.pry
+    jobs = ["Data Analyst", "Data Scientist", "Mobile Developer", "QA Engineer", "Software Engineer", "Systems Administrator", "Web Developer"]
+    salaries = []
+    @service[:salaries].map do |salary|
+      if jobs.include?(salary[:job][:title])
+        title = salary[:job][:title]
+        min = salary[:salary_percentiles][:percentile_75]
+        max = salary[:salary_percentiles][:percentile_25]
+        salaries << {title: title, min: min, max: max}
+      end
+    end
+    salaries
   end
 end
--- spec/facades/salaries_facade_spec.rb
+++ spec/facades/salaries_facade_spec.rb
@@ -4,10 +4,19 @@ RSpec.describe SalariesFacade do
   describe "instance methods" do
     it "salary_info" do
       VCR.use_cassette("facade_chicago_salaries") do
-        search = SalariesFacade.new("chicago")
-        require 'pry'; binding.pry
-        expect(search.salary_info).to be_a(Array)
-        expect(search.salary_info.first).to be_a(Salary)
+        search = SalariesFacade.salary_info("chicago")
+
+        expect(search).to be_a(Array)
+        expect(search.first).to be_a(Hash)
+
+        expect(search.first).to have_key(:title)
+        expect(search.first[:title]).to be_a(String)
+
+        expect(search.first).to have_key(:min)
+        expect(search.first[:min]).to be_a(Float)
+
+        expect(search.first).to have_key(:max)
+        expect(search.first[:max]).to be_a(Float)
       end
     end
   end
