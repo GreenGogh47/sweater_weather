056605ee9d8232d18f5c7b6ee55977ec827c2919
Chris Crane <chrisjcrane@gmail.com>
2023-04-25 10:38:20 -0600

    update user request spec

--- spec/requests/v1/users_request_spec.rb
+++ spec/requests/v1/users_request_spec.rb
@@ -13,10 +13,17 @@ RSpec.describe "Users API" do
     VCR.use_cassette('user_creation') do
       post "/api/v1/users", params: @params.to_json, headers: { 'CONTENT_TYPE' => 'application/json' }
 
-      require 'pry'; binding.pry
-
       expect(response).to be_successful
       expect(response.status).to eq(201)
+
+      expect(response.body).to be_a(String)
+      expect(response.body).to include("data")
+      expect(response.body).to include("users")
+      expect(response.body).to include("email")
+      expect(response.body).to include("api_key")
+
+      expect(response.body).to_not include("password")
+      expect(response.body).to_not include("password_confirmation")
     end
   end
 end
