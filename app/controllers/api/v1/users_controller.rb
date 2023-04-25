705fd7ab28d46f105b21188d3591d50a9b6060a6
Chris Crane <chrisjcrane@gmail.com>
2023-04-24 22:11:18 -0600

    install shoulda-matchers and add strong params to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::UsersController < ApplicationController
   def create
     User.new(user_params)
   end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
 end
