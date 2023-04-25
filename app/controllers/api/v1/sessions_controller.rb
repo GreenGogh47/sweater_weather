b816cc5aee5f4206ca6e62d318b0a9e9ce597bd1
Chris Crane <chrisjcrane@gmail.com>
2023-04-25 11:32:53 -0600

    pass sad path for sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    if user.email == params[:email] && user.password == params[:password]
+    if user && user.authenticate(params[:password])
       render json: UsersSerializer.new(user)
     else
       render json: {error: "Incorrect credentials"}, status: 400
