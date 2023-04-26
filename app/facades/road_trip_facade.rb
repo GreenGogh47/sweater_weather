20cfb3f77ee51ecba55de2b078ecdd02a24a55e1
Chris Crane <chrisjcrane@gmail.com>
2023-04-25 18:30:59 -0600

    refactor roat drip facade to create a RoadTrip for time and day of arrival

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -5,41 +5,18 @@ class RoadTripFacade
     road_trip = DirectionsService.new.directions(from, to)
     forecast = ForecastFacade.forecast(to)
     
-    travel_time = Time.parse(road_trip[:route][:formattedTime])
+    travel_time = road_trip[:route][:time]
     end_time = Time.now + travel_time
+    formatted_travel_time = road_trip[:route][:formattedTime]
 
-    forecast.hourly_weather.each do |hour|
-      time = Time.parse(hour[:time])
-      if hour[:time] >= end_time && hour[:time] <= end_time + 1.hour
-        @road_trip = RoadTrip.new(from, to, travel_time, hour)
+    forecast.hourly_weather.each do |day|
+      day[:hour].each do |time|
+        if time[:time].include?(end_time.strftime('%Y-%m-%d'))
+          if end_time.strftime('%H') == Time.parse(time[:time]).strftime('%H')
+            @road_trip = RoadTrip.new(from, to, formatted_travel_time, time)
+          end
+        end
       end
     end
   end
 end
-
-# class RoadTripFacade
-#   attr_reader :road_trip
-
-#   def self.directions(from, to)
-#     road_trip = DirectionsService.new.directions(from, to)
-#     travel_time = Time.zone.parse(road_trip[:route][:formattedTime])
-#     end_time = Time.zone.now + travel_time
-    
-#     forecast = ForecastFacade.forecast(to)
-    
-#     forecast.hourly_weather.each do |hour|
-#       time = Time.zone.parse(hour[:time])
-#       require 'pry'; binding.pry
-#       if time >= end_time && time <= end_time + 1.hour
-#         @road_trip = RoadTrip.new(from, to, travel_time, hour)
-#       end
-#     end
-#   end
-# end
-
-# I need forecast data for days other than just today.
-
-
-
-
-
